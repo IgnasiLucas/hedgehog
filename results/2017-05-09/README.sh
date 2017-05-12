bdf7bda92f5aa2d3f987d786793151dc191c1ad6
ignasi <ignasilucas@gmail.com>
2017-05-12 18:37:53 +0200

    Alignment is reasonably good as it is

--- results/2017-05-09/README.sh
+++ results/2017-05-09/README.sh
@@ -30,27 +30,27 @@ SAMPLE=(Er26_JUG4 Er27_SK32 Er28_112   Er29_122  Er30_79    Er31_453  Er32_183  
 
 
 for i in `seq 0 24`; do
-   if [ ! -e ${SAMPLE[$i]}.fastq ] && [ ! -e read1.fastq ]; then
+   if [ ! -e ${SAMPLE[$i]}.fastq ] && [ ! -e whole.fastq ]; then
       vsearch --fastx_subsample $FASTQ_I_DIR/${SAMPLE[$i]}_1.fastq.gz --fastqout ${SAMPLE[$i]}.fastq --sample_size 1000 &
    fi
 done
 
 for i in `seq 25 49`; do
-   if [ ! -e ${SAMPLE[$i]}.fastq ] && [ ! -e read1.fastq ]; then
-      vsearch --fastx_subsample $FASTQ_II_DIR/runII_fastq/${SAMPLE[$i]}_1.fastq.gz --fastqout ${SAMPLE[$i]}.fastq --sample_size 1000 &
+   if [ ! -e ${SAMPLE[$i]}.fastq ] && [ ! -e whole.fastq ]; then
+      vsearch --fastx_subsample $FASTQ_II_DIR/${SAMPLE[$i]}_1.fastq.gz --fastqout ${SAMPLE[$i]}.fastq --sample_size 1000 &
    fi
 done
 wait
 
-if [ ! -e read1.fastq ]; then
-   cat Er*.fastq > read1.fastq
+if [ ! -e whole.fastq ]; then
+   cat Er*.fastq > whole.fastq
    rm Er*.fastq
 fi
 
 # I trim the reads with gawk because I notice some are already trimmed, and therefore
 # I cannot just use the seqcrumb trim_edges.
 if [ ! -e trimmed.fastq ]; then
-   gawk '{$1 = substr($1,1,80); print}' read1.fastq > trimmed.fastq
+   gawk '{$1 = substr($1,1,80); print}' whole.fastq > trimmed.fastq
 fi
 
 if [ ! -e bowtie1 ]; then mkdir bowtie1; fi
@@ -82,12 +82,82 @@ fi
 EXE=($BOWTIE $BOWTIE2)
 FOLDER=(bowtie1 bowtie2)
 for i in 0 1; do
-   for j in read1 trimmed; do
+   for j in whole trimmed; do
       for k in sensitive conservative; do
          if [ ! -d ${FOLDER[$i]}/$j.$k ]; then
             mkdir ${FOLDER[$i]}/$j.$k
          fi
-         align.sh ${FOLDER[$i]} ${EXE[$i]} $j.fastq $k ${FOLDER[$i]}/$j.$k
+         if [ ! -e ${FOLDER[$i]}/$j.$k/output.log ]; then
+            ./align.sh ${FOLDER[$i]} ${EXE[$i]} $j.fastq $k ${FOLDER[$i]}/$j.$k 2> ${FOLDER[$i]}/$j.$k/output.log
+#         else
+#            echo "${FOLDER[$i]}/$j.$k/output.log already present."
+#            exit
+         fi
       done
    done
 done
+
+if [ ! -e summary.txt ]; then
+   echo -e "Aligner\tInput\tSetting\tUnique\tMultiple\tUnmapped" > summary.txt
+   for i in whole trimmed; do
+      for j in conservative sensitive; do
+         gawk -v INPUT=$i -v SETTING=$j '(/reads processed:/){
+            TOTAL = $4
+         }(/reads with at least one reported alignment:/){
+            UNIQUE = $9
+         }(/reads that failed to align:/){
+            UNMAPPED = $7
+         }(/reads with alignments suppressed due to -m:/){
+            MULTIPLE = $9
+         }END{
+            printf "Bowtie1\t%s\t%s\t%.2f%%\t%.2f%%\t%.2f%%\n", INPUT, SETTING, 100 * (UNIQUE + 0) / TOTAL, 100 * (MULTIPLE + 0) / TOTAL, 100 * (UNMAPPED + 0) / TOTAL
+         }' bowtie1/$i.$j/output.log >> summary.txt
+      done
+   done
+   for i in whole trimmed; do
+      for j in conservative sensitive; do
+         gawk -v INPUT=$i -v SETTING=$j '(/reads; of these:/){
+            TOTAL = $1
+         }(/aligned exactly 1 time/){
+            UNIQUE = $1
+         }(/aligned 0 times/){
+            UNMAPPED = $1
+         }(/aligned >1 times/){
+            MULTIPLE = $1
+         }END{
+            printf "Bowtie2\t%s\t%s\t%.2f%%\t%.2f%%\t%.2f%%\n", INPUT, SETTING, 100 * (UNIQUE + 0) / TOTAL, 100 * (MULTIPLE + 0) / TOTAL, 100 * (UNMAPPED + 0) / TOTAL
+         }' bowtie2/$i.$j/output.log >> summary.txt
+      done
+   done
+fi
+
+# CONCLUSIONS
+# ===========
+#
+# Summary table:
+#
+# --------------------------------------------------------------
+# Aligner	Input	Setting 	Unique	Multip.	Unmapped
+# --------------------------------------------------------------
+# Bowtie1	whole	conservative	46.41%	6.57%	47.02%
+# Bowtie1	whole	sensitive	59.73%	0.00%*	40.27%
+# Bowtie1	trimmed	conservative	48.96%	13.22%	37.81%
+# Bowtie1	trimmed	sensitive	69.10%	0.00%*	30.90%
+# Bowtie2	whole	conservative	47.22%	34.32%	18.46%
+# Bowtie2	whole	sensitive	45.51%	32.63%	21.86%
+# Bowtie2	trimmed	conservative	36.70%	47.03%	16.26%
+# Bowtie2	trimmed	sensitive	36.36%	45.98%	17.66%
+# --------------------------------------------------------------
+#
+# * In the sensitive setting of bowtie1 the -m options, which removes ambiguously mapped reads
+# was not applied. Thus, there are no multiply mapped reads identified, but they are actually
+# counted together with the uniquely mapped.
+#
+# In general, the lower percentage of unmapped reads with Bowtie2 confirms that Bowtie2 is
+# more sensitive than Bowtie1. However, it is clear that the higher sensitivity only helps
+# finding secondary mappings to many reads.
+#
+# The current mappings, made with Bowtie1 produced an amount of uniquely mapped reads similar
+# to the best results tested here. Thus, it is not necessary to try to improve the mapping.
+# Instead of increasing coverage, we would increase the noise caused by ambiguously mapped
+# reads.
--- results/2017-05-09/align.sh
+++ results/2017-05-09/align.sh
@@ -13,15 +13,20 @@
 #    ${FOLDER[$i]}/$j.$k:   the subfolder where to sent the output.
 #
 
+if [ $1 == "-h" ]; then
+   echo "./align.sh <index folder> <path to bowtie or bowtie2> <input fastq> {sensitive | conservative} <output dir>"
+   exit
+fi
+
 if [ $1 == bowtie1 ]; then
    if [ $4 == sensitive ]; then
       if [ ! -e bowtie1/trimmed.sensitive/output.bam ]; then
          # I request the best alignment among those with no more than 3 mismatches in the first 60
          # bases, and with a total base quality sum of mismatching bases not higher than 80.
-         $2 -n 3 -l 60 -e 80 --best -p 20 --nomaqround -t -S --sam-nohead bowtie1/erinaceus $3 | samtools view -Sb - > $5/output.bam
+         $2 -n 3 -l 60 -e 80 --best -p 20 --nomaqround -t -S bowtie1/erinaceus $3 | samtools view -Sb - > $5/output.bam
       fi
       if [ ! -e bowtie1/whole.sensitive/output.bam ]; then
-         $2 -n 3 -l 60 -e 80 --best -p 20 --nomaqround -t -S --sam-nohead bowtie1/erinaceus $3 | samtools view -Sb - > $5/output.bam
+         $2 -n 3 -l 60 -e 80 --best -p 20 --nomaqround -t -S bowtie1/erinaceus $3 | samtools view -Sb - > $5/output.bam
       fi
    else
       if [ $4 == conservative ]; then
@@ -29,10 +34,10 @@ if [ $1 == bowtie1 ]; then
             # In the conservative setting, I request no more than 1 mismatch in the first 50 bases,
             # with a total base quality sum of mismatching bases not higher than 60. I also dismiss
             # ambiguous mappings.
-            $2 -n 1 -l 50 -e 60 -m 1 -p 20 --nomaqround -t -S --sam-nohead bowtie1/erinaceus $3 | samtools view -Sb - > $5/output.bam
+            $2 -n 1 -l 50 -e 60 -m 1 -p 20 --nomaqround -t -S bowtie1/erinaceus $3 | samtools view -Sb - > $5/output.bam
          fi
          if [ ! -e bowtie1/whole.conservative/output.bam ]; then
-            $2 -n 1 -l 50 -e 60 -m 1 -p 20 --nomaqround -t -S --sam-nohead bowtie1/erinaceus $3 | samtools view -Sb - > $5/output.bam
+            $2 -n 1 -l 50 -e 60 -m 1 -p 20 --nomaqround -t -S bowtie1/erinaceus $3 | samtools view -Sb - > $5/output.bam
          fi
       fi
    fi
@@ -42,19 +47,19 @@ else
          if [ ! -e bowtie2/trimmed.sensitive/output.bam ]; then
             # I request up to 1 mismatch in 20 bp long seeds. This is between the sensitive and
             # the very sensitive preset options.
-            $2 -N 1 -L 20 -D 25 -R 2 -i S,1,1 -t --no-unal --no-hd -p 20 -x bowtie2/erinaceus -U $3 | samtools view -Sb - > $5/output.bam
+            $2 -N 1 -L 20 -D 25 -R 2 -i S,1,1 -t --no-unal -p 20 -x bowtie2/erinaceus -U $3 | samtools view -Sb - > $5/output.bam
          fi
          if [ ! -e bowtie2/whole.sensitive/output.bam ]; then
-            $2 -N 1 -L 20 -D 25 -R 2 -i S,1,1 -t --no-unal --no-hd -p 20 -x bowtie2/erinaceus -U $3 | samtools view -Sb - > $5/output.bam
+            $2 -N 1 -L 20 -D 25 -R 2 -i S,1,1 -t --no-unal -p 20 -x bowtie2/erinaceus -U $3 | samtools view -Sb - > $5/output.bam
          fi
       else
          if [ $4 == conservative ]; then
             if [ ! -e bowtie2/trimmed.conservative/output.bam ]; then
                # Zero mismatches in 22-bp long seeds.
-               $2 -N 0 -L 22 -D 10 -R 2 -i S,1,1.15 -t --no-unal --no-hd -p 20 -x bowtie2/erinaceus -U $3 | samtools view -Sb - > $5/output.bam
+               $2 -N 0 -L 22 -D 10 -R 2 -i S,1,1.15 -t --no-unal -p 20 -x bowtie2/erinaceus -U $3 | samtools view -Sb - > $5/output.bam
             fi
             if [ ! -e bowtie2/whole.conservative/output.bam ]; then
-               $2 -N 0 -L 22 -D 10 -R 2 -i S,1,1.15 -t --no-unal --no-hd -p 20 -x bowtie2/erinaceus -U $3 | samtools view -Sb - > $5/output.bam
+               $2 -N 0 -L 22 -D 10 -R 2 -i S,1,1.15 -t --no-unal -p 20 -x bowtie2/erinaceus -U $3 | samtools view -Sb - > $5/output.bam
             fi
          fi
       fi
