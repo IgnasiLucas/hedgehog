ac5067c01d09787ab067b9ff1600a78d52829b6d
ignasi <ignasilucas@gmail.com>
2018-08-29 23:09:49 +0200

    Finished the analysis of the effect of PCR duplicates. Potentially causing up to 16% of variable sites with deficit of heterozygous genotypes within populations.

--- results/2018-08-09/README.sh
+++ results/2018-08-09/README.sh
@@ -111,10 +111,19 @@ fi
 # balance per individual, across sites. The rationale here is that some individuals may
 # have more unbalanced allele observations than others, while all individuals from the
 # same population are expected to have similar genome-wide heterozygosities under random
-# mating. Plus, looking at individuals separately, I can distinguish the effect of PCR
-# duplicates from the effect of low coverage, by setting a minimum coverage. In addition,
-# I will not look only at genotypes declared heterozygous, but also at those considered
-# homozygous even though more than one allele was observed.
+# mating. Note, however, that all individuals have reads from different libraries, the
+# first ones with high levels of duplicates, and subsequent ones with less (see below).
+#
+# The random deviation from the 50% expectation due to low coverage makes one of the alleles
+# look more frequent then the other. The same happens with PCR duplicates. Because in both
+# cases the allele that gets sampled more often at each site is randomly chosen, I should
+# not see an overall deviation from the 50% balance between reference and alternative
+# allele. Instead, I must identify the alleles by how frequently they are observed, as
+# 'minor' and 'major', at each site. If only low coverage was causing dispersion, 50%
+# would still be the average. PCR duplicates, however, will cause the average to drop.
+# I can estimate the overall average frequency of the least observed allele by adding
+# up allele observations across heterozygous sites. That will give an accurate mean.
+# I can also limit the effect of low coverage by setting a minimum coverage.
 
 MINCOV=5
 if [ ! -e AB_perSample.hist ]; then
@@ -151,8 +160,12 @@ if [ ! -e AB_perSample.hist ]; then
             if ((ALLELECOUNT[1] != ".") && (COV >= MINCOV)) {
                NUMSITES[SAMPLE[i]]++
                GTFREQ[SAMPLE[i]][SAMPLEINFO[GT]]++
-               if (ALLELECOUNT[1] < ALLELECOUNT[2]) {MINOR=1; MAJOR=2} else {MINOR=2; MAJOR=1}
-               AB[SAMPLE[i]][sprintf("%.2f", ALLELECOUNT[MINOR] / COV)]++
+               if (ALLELECOUNT[1] <= ALLELECOUNT[2]) {MINOR=1; MAJOR=2} else {MINOR=2; MAJOR=1}
+               AB[SAMPLE[i]][sprintf("%.1f", ALLELECOUNT[MINOR] / COV)]++
+               if (SAMPLEINFO[GT] == "0/1") {
+                  MINORCOV[SAMPLE[i]] += ALLELECOUNT[MINOR]
+                  TOTALCOV[SAMPLE[i]] += COV
+               }
             }
          }
       } END {
@@ -169,17 +182,28 @@ if [ ! -e AB_perSample.hist ]; then
          LINE = "#Heterozygosity1"
          for (i = 1; i <= NUMSAMPLES; i++) {
             LINE = LINE sprintf("\t%.4f", GTFREQ[SAMPLE[ORDER[i]]]["0/1"] / NUMSITES[SAMPLE[ORDER[i]]])
+            # This is the heterozygosity as actually reported in the vcf file, counting the believed heterozygous sites.
          }
          print LINE
          LINE = "#Heterozygosity2"
          for (i = 1; i <= NUMSAMPLES; i++) {
-            LINE = LINE sprintf("\t%.4f", (NUMSITES[SAMPLE[ORDER[i]]] - AB[SAMPLE[ORDER[i]]]["0.00"]) / NUMSITES[SAMPLE[ORDER[i]]])
+            LINE = LINE sprintf("\t%.4f", (NUMSITES[SAMPLE[ORDER[i]]] - AB[SAMPLE[ORDER[i]]]["0.0"]) / NUMSITES[SAMPLE[ORDER[i]]])
+            # This heterozygosity counts as heterozygous sites with at least 1 different allele observation in 10 reads.
          }
          print LINE
-         for (ab = 0; ab <= 0.50; ab += 0.01) {
-            LINE = sprintf("%.2f", ab)
+         LINE = "#Average_AB"
+         for (i = 1; i <= NUMSAMPLES; i++) {
+            if (TOTALCOV[SAMPLE[ORDER[i]]] > 0) {
+               LINE = LINE sprintf("\t%.6f", MINORCOV[SAMPLE[ORDER[i]]] / TOTALCOV[SAMPLE[ORDER[i]]])
+            } else {
+               LINE = LINE "\tnan"
+            }
+         }
+         print LINE
+         for (ab = 0.0; ab <= 0.51; ab += 0.1) {
+            LINE = sprintf("%.1f", ab)
             for (i = 1; i <= NUMSAMPLES; i++) {
-               LINE = LINE sprintf("\t%6i", AB[SAMPLE[ORDER[i]]][sprintf("%.2f", ab)] + 0)
+               LINE = LINE sprintf("\t%6i", AB[SAMPLE[ORDER[i]]][sprintf("%.1f", ab)] + 0)
             }
             print LINE
          }
@@ -191,7 +215,7 @@ fi
 # further from HWE.
 
 for pop in europaeus roumanicus concolor; do
-   if [ ! -e $pop.hwe ]; then
+   if [ ! -e $pop.hwe ] && [ ! -e $pop'_hwe.summary' ]; then
       vcftools --vcf $VCFFILE \
                --out $pop \
                --keep $pop.txt \
@@ -201,10 +225,75 @@ done
 
 wait
 
+for  pop in europaeus roumanicus concolor; do
+   if [ ! -e $pop'_hwe.summary' ]; then
+      cut -f 3- $pop.hwe | grep -vP "nan|HWE" | gawk '{F[$0]++}END{for (f in F) print f "\t" F[f]}' | LC_ALL=C sort -grk 7 -k 3 > $pop'_hwe.summary'
+   fi
+done
+
+if [ ! -e summary_hwe.txt ]; then
+   echo "# These are numbers of sites polymorphic within a population that show significatn deficit" > summary_hwe.txt
+   echo "# or excess of heterozygous individuals, or not, at 0.05 significance level, according to" >> summary_hwe.txt
+   echo "# the tests performed by vcftools. Sites may be counted more than once, when polymorphic in" >> summary_hwe.txt
+   echo "# more than one population." >> summary_hwe.txt
+   gawk '($5 < 0.05){HET_DEF += $7}
+      ($6 < 0.05){HET_EXC += $7}
+      (($5 >= 0.05) && ($6 >= 0.05)){HWE += $7}
+      END{
+         TOTAL = HET_DEF + HET_EXC + HWE
+         printf("Heterozygous deficit:%7i (%3.4f\%)\nHeterozyous excess:%9i (%3.4f\%)\nIn HW equilibrium:%10i (%3.4f\%)\n", HET_DEF, 100 * HET_DEF / TOTAL, HET_EXC, 100 * HET_EXC / TOTAL, HWE, 100 * HWE / TOTAL)
+      }' *_hwe.summary >> summary_hwe.txt
+fi
+
+if [ ! -e summary_duplicate_reads.txt ]; then
+   echo -e "#Library\tTotal_reads\tDuplicates\tPercentage" > summary_duplicate_reads.txt
+   gawk -F "\t" '(($1 > 2) && ($1 <= 200)){
+      DUPS[FILENAME] += $2
+   }(NF == 3){
+      TOTAL[FILENAME] = $3
+   }END{
+      for (f in DUPS) {
+         LIBRARY = f
+         gsub(/_dups.hist/, "", LIBRARY)
+         print LIBRARY "\t" TOTAL[f] "\t" DUPS[f] "\t" 100 * DUPS[f] / TOTAL[f]
+      }
+   }' *.hist | sort -k 1,1 >> summary_duplicate_reads.txt
+fi
 
 # CONCLUSIONS
 #
-# I don't know why I'm not printing AB=0.5 for each sample. In any case, the dispersion of
-# AB values per sample makes it clear that I should use an empirical Bayes approach: the
-# prior AB is 0.5, and the qüestion is if there is much data to believe a departure from
-# that expectation.
+# From the bam files, I counted as duplicates the second reads that are aligned to the
+# same position. I built the histograms of number of duplicates per positions, since it
+# is very different to have 1000 duplicates from the same or from 500 different positions.
+# I only count as problematic duplicates those that repeat a site more than twice and less
+# than 201. The rationale is that adding one read to a locus will not bias much the allele
+# frequency. And adding more than 200 duplicated reads to a locus will increase the coverage
+# of all variant genotypes there above the threshold used to filter genotypes by coverage.
+# The file "summary_duplicate_reads.txt" shows an important batch effect. The number of
+# problematic duplicates drops with subsequent library preparations. The first 25 samples
+# from 2016 have the highest levels of duplicates (between 5.8 and 7.8 %). The subsequent
+# 25 samples from 2016 have 2.1 and 3.2 % duplicates. And all samples from 2018 have only
+# between 0.4 and 0.7%.
+#
+# The allele balance (AB) in heterozygous sites is the proportion of each allele's observations.
+# It should be around 50% of each allele. PCR duplicates increase the proportion of observations
+# of one allele, biasing the AB at each site. The vcf file reports a measure of AB, defined as
+# follows: "Allele balance at heterozygous sites: a number between 0 and 1 representing the
+# ratio of reads showing the reference allele to all reads, considering only reads from individuals
+# called as heterozygous". The average AB reported in the vcf file is 0.48 (see file 'AB.hist'),
+# giving the false impression that PCR duplicates do not affect it. However, the allele that
+# gets amplified by PCR duplicates is not always the same. The individual biases balance out
+# when combining coverage data from several heterozygous individuals. The way to use AB as an
+# indicator of PCR duplicates is per individual (see file 'AB_perSample.hist'. Then we see that
+# the average proportion of observations of the least observed allele in heterozygous sites
+# ranges from 0.21 to 0.36 among individuals. That's much less than the 50% expected without
+# PCR duplicates.
+#
+# This suggests that several heterozygous sites are erroneously considered homozygous, because
+# of undersampling of one allele. Granted that the allele missed is not always the same, so
+# that the allele frequencies are still estimated accurately. But the genotype proportions and
+# observed heterozygosities are biased.
+#
+# The Hardy-Weinberg statistics reported by vcftools (see 'summary_hwe.txt') show that 16% of
+# sites polymorphic within a population show a significant (p value < 0.05) deficit of heterozygous
+# genotypes. Only 0.7 % shows a significant excess.
