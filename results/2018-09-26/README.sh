38b6314a2da70529aa9a6ae14f8b9f512f3f58f0
ignasi <ignasilucas@gmail.com>
2018-10-08 23:31:59 +0200

    Revised again the filtering to produce two different vcf files with at least 10 roumanicus, 10 europaeus, and 4 concolor in every site: one with all Erinaceus individuals for the abba/baba test, and one with only the 41 that have less than 63% missing data, for Admixture.

--- results/2018-09-26/README.sh
+++ results/2018-09-26/README.sh
@@ -6,48 +6,105 @@
 # I want to re-do the filtering of the vcf file. We need to insert the binary
 # presence flag, and then make sure that we select for analysis sites where at least
 # four individuals from E. concolor have data.
+#
+# After giving it some thought, it becomes clear that the Admixture analysis and
+# the abba/baba or D tests have different requirements: Admixture estimates allele
+# frequencies, and benefits from large numbers of individuals per population. The
+# abba/baba test does not seem so sensitive to allele frequencies: even though current
+# implementation uses allele frequencies in the sample, it was originally designed to
+# work even with single individuals, and it gains power from the number of SNPs used,
+# more than from the number of individuals, since it is estimating a proportion of
+# SNPs in the genome.
+#
+# I will produce two filterings, one optimized for Admixture, and one for the abba/baba.
 
 GZVCF=/data/kristyna/hedgehog/results_2018/23-02-2018/merged.vcf.gz
 ADD_FLAG_DIR=../../bin
-INDLIST=/data/kristyna/hedgehog/results_2018/05-06-2018/r10e10c4/popmap_erinaceus
+MAX_MISSING=63
 
-if [ ! -e IndList.txt ]; then
-   # This file is just the list of samples to be used, after removing some with low
-   # quality data and excluding the two samples from diferen genera (Atelerix and Hemiechinus).
-   cut -f 1 $INDLIST > IndList.txt
+if [ ! -e NotErinaceus.txt ]; then
+   # The Atelerix and Hemiechinus individuals cannot take part in the Admixture analysis,
+   # and should be excluded.
+   echo Er65_IS25 >  NotErinaceus.txt
+   echo Er73_SNG1 >> NotErinaceus.txt
 fi
-for rou in 10 15; do
-   for eur in 10 15; do
-      for con in 4 5; do
-         if [ ! -e r${rou}e${eur}c${con}.vcf ]; then
-            if [ ! -e flagged.vcf ]; then
-               vcftools --gzvcf $GZVCF \
-                  --stdout \
-                  --keep IndList.txt \
-                  --maf 0.01 \
-                  --max-maf 0.95 \
-                  --remove-indels \
-                  --min-alleles 2 \
-                  --max-alleles 2 \
-                  --maxDP 200 \
-                  --minDP 4 \
-                  --minQ 50 \
-                  --thin 261 \
-                  --recode \
-                  --recode-INFO-all | \
-               gawk -f $ADD_FLAG_DIR/add_flag.awk > flagged.vcf
-            fi
-            # It would be faster to pipe the output of add_flag.awk script to the next
-            # script. But I want to keep the flagged.vcf file, in case we need a different
-            # filtering strategy. The name of the output file encodes the minimum number
-            # of individuals requested in each species. E.g., r10e10c5.vcf means 10 roumanicus,
-            # 10 europaeus, and 5 concolor.
-            gawk -f filtervcf.awk -v ROU=$rou -v EUR=$eur -v CON=$con popmap.txt flagged.vcf > r${rou}e${eur}c${con}.vcf &
-         fi
-      done
-   done
-done
-wait
+
+# The binary flags that indicate presence of alleles in individuals are only meaningful
+# as long as the individuals keep their order in the vcf file. Thus, any exclusion
+# of individuals should be made before the binary flags are added. Thus, I will have to
+# add the flags twice: first to the original vcf file, to be able to filter sites by
+# composition, and then again after excluding the individuals with too much missing data.
+# Note that I need to determine the amount of missing data on a vcf file already filtered
+# by composition. Otherwise, there is over 1.4 million sites and most individuals have
+# very high levels of missing data.
+
+if [ ! -e Erinaceus_flagged.vcf ]; then
+   vcftools --gzvcf $GZVCF \
+            --stdout \
+            --remove NotErinaceus.txt \
+            --maf 0.01 \
+            --max-maf 0.95 \
+            --remove-indels \
+            --min-alleles 2 \
+            --max-alleles 2 \
+            --maxDP 200 \
+            --minDP 4 \
+            --minQ 50 \
+            --thin 261 \
+            --recode \
+            --recode-INFO-all | \
+   gawk -f $ADD_FLAG_DIR/add_flag.awk > Erinaceus_flagged.vcf
+fi
+
+if [ ! -e Erinaceus_r10e10c4.vcf ]; then
+   gawk -f filtervcf.awk -v ROU=10 -v EUR=10 -v CON=4 popmap.txt Erinaceus_flagged.vcf > Erinaceus_r10e10c4.vcf
+fi
+
+if [ ! -e missingness.txt ]; then
+   gawk '(/^#CHROM/){
+      for (i = 10; i <= NF; i++) {
+         SAMPLE[i] = $i
+      }
+   }(/^[^#]/){
+      for (i = 10; i <= NF; i++) {
+         if ($i ~ /^\.$|\.\/\./) {
+            MISSING[SAMPLE[i]] += 1
+         }
+      }
+      TOTAL += 1
+   }END{
+      for (i in SAMPLE) {
+         printf("%s\t%.4f\n", SAMPLE[i], MISSING[SAMPLE[i]] / TOTAL)
+      }
+   }' Erinaceus_r10e10c4.vcf | sort -nrk 2,2 > missingness.txt
+fi
+
+if [ ! -e missing_$MAX_MISSING.txt ]; then
+   gawk -v MAX=$MAX_MISSING.txt '($2 > MAX/100){print $1}' missingness.txt > missing_$MAX_MISSING.txt
+fi
+
+if [ ! -e Erinaceus_MaxMissing$MAX_MISSING\_flagged.vcf ]; then
+   vcftools --gzvcf $GZVCF \
+            --stdout \
+            --remove NotErinaceus.txt \
+            --remove missing_$MAX_MISSING.txt \
+            --maf 0.01 \
+            --max-maf 0.95 \
+            --remove-indels \
+            --min-alleles 2 \
+            --max-alleles 2 \
+            --maxDP 200 \
+            --minDP 4 \
+            --minQ 50 \
+            --thin 261 \
+            --recode \
+            --recode-INFO-all | \
+   gawk -f $ADD_FLAG_DIR/add_flag.awk > Erinaceus_MaxMissing$MAX_MISSING\_flagged.vcf
+fi
+
+if [ ! -e ErinMaxMiss$MAX_MISSING\_r10e10c4.vcf ]; then
+   gawk -f filtervcf.awk -v ROU=10 -v EUR=10 -v CON=4 popmap.txt Erinaceus_MaxMissing$MAX_MISSING\_flagged.vcf > ErinMaxMiss$MAX_MISSING\_r10e10c4.vcf
+fi
 
 if [ ! -e summary.txt ]; then
    for vcf in `ls -1 *.vcf`; do
@@ -68,7 +125,6 @@ if [ ! -e summary.txt ]; then
             }
          }
          if ($0 ~ /^[^#]/) {
-#            N["roumanicus"] = 0; N["europaeus"] = 0; N["concolor"] = 0; N["Hemiechinus"] = 0; N["wrong"] = 0
             delete(N)
             for (i = 10; i <= NF; i++) {
                if ($i !~ /\.\/\./) {
