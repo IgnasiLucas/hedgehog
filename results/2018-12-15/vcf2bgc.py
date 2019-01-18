cc9d2a89e5b415cfba9043d62d477d8ce2d0317f
ignasi <ignasilucas@gmail.com>
2019-01-19 00:11:37 +0100

    The parser is almost done, but tests with bgc give an error.

--- results/2018-12-15/vcf2bgc.py
+++ results/2018-12-15/vcf2bgc.py
@@ -4,15 +4,7 @@ import argparse
 # the MIT license. Similar to pyvcf, but newer, and less tested.
 import vcfpy
 
-def parse_popmap(popmap):
-   '''Creates a dictionary with sample names as keys and population names as values.'''
-   table = {}
-   for line in popmap:
-      if not line.startswith('#'):
-         table[line.split()[0]] = line.split()[1]
-   return table
-
-def make_poplist(popmap):
+def MakePopDict(popmap):
    '''Creates a dictionary with population name as key and a list of sample names as value.'''
    table = {}
    for line in popmap:
@@ -28,29 +20,70 @@ def main():
    parser = argparse.ArgumentParser(description = 'Takes a vcf file and a population file and produces the input files required for Bayesian Genomic Clines estimation (bgc), namely the two parental populations data files and the admixed population(s) data file. The genetic map is not implemented yet.')
    parser.add_argument('-0', '--pop0', type=str, default='roumanicus', help='Name of parental population 0, as in the populations mapping file. Default: roumanicus.')
    parser.add_argument('-1', '--pop1', type=str, default='europaeus', help='Name of parental population 1, as in the population mapping file. Default: europaeus.')
-   parser.add_argument('-a', '--admixed', type=str, action='append', default='admixed', help='Name of an admixed population, as in the population mapping file. Repeat to add more than one. Default: admixed.')
+   parser.add_argument('-a', '--admixed', action='append', help='Name of an admixed population, as in the population mapping file. Repeat to add more than one. Default: admixed.')
    parser.add_argument('-p', '--prefix', type=str, default="", help='Prefix to be added to the predefined output names, which are based on the names of the populations. For example, to save output in a particular folder.')
    parser.add_argument('-u', '--uncertain', action='store_true', help='Genotypes are uncertain, and an estimate of sequencing errors must be included in output. Requires specification of the error through -e. Default: false.')
    parser.add_argument('-e', '--errors', type=float, help='Common sequencing error value for all sites. If zero, site-specific error values will be estimated from vcf statistics, if available, provided -u. If -u is not used, -e is ignored. Default: 0.0.')
    parser.add_argument('-v', '--vcf', type=argparse.FileType('r'), default='-', help='Input vcf file. Default: standard input.')
    parser.add_argument('popmap', type=argparse.FileType('r'), help='Input populations mapping file, tab or space separated: sample name and population.' )
    args = parser.parse_args()
-#   PopulationDict = parse_popmap(args.popmap)
-   PopulationList = make_poplist(args.popmap)
-   assert args.pop0 in PopulationDict.values()
-   assert args.pop1 in PopulationDict.values()
-   assert args.admixed in PopulationDict.values()
+   SamplesInPop = MakePopDict(args.popmap)
+   assert args.pop0 in SamplesInPop.keys()
+   assert args.pop1 in SamplesInPop.keys()
+   for pop in args.admixed:
+      assert pop in SamplesInPop.keys()
    pop0 = open(args.prefix + 'pop0.txt', 'w')
    pop1 = open(args.prefix + 'pop1.txt', 'w')
    admixed = open(args.prefix + 'admixed.txt', 'w')
-   reader = vcfpy.Reader.from_stream(vcf)
+   reader = vcfpy.Reader.from_stream(args.vcf)
+   # I want to remove from the populations' lists of samples the samples that are not present in the vcf.
+   for pop in SamplesInPop.keys():
+      tmp = [sample for sample in SamplesInPop[pop] if sample in reader.header.samples.names]
+      SamplesInPop[pop] = tmp
    if args.uncertain:
       if args.errors == 0.0:
-
+         pass
       elif args.errors > 0.0:
-
+         pass
       else:
+         pass
    else:
+      locus = 0
       for record in reader:
-         for sample in PopulationList[args.pop0]:
-            genotype = record.call_for_sample[sample].data.get('GT')
+         # I need to skip incomplete records, to limit the amount of loci
+         if './.' in [call.data.get('GT') for call in record.calls]:
+            continue
+         P0RefCount = 0
+         P0AltCount = 0
+         P1RefCount = 0
+         P1AltCount = 0
+         for sample in SamplesInPop[args.pop0]:
+            P0RefCount += record.call_for_sample[sample].data.get('GT').count('0')
+            P0AltCount += record.call_for_sample[sample].data.get('GT').count('1')
+         for sample in SamplesInPop[args.pop1]:
+            P1RefCount += record.call_for_sample[sample].data.get('GT').count('0')
+            P1AltCount += record.call_for_sample[sample].data.get('GT').count('1')
+#        admixed.write("{}:{}\n".format(record.CHROM, record.POS))
+         admixed.write("locus {}\n".format(locus))
+         for pop in args.admixed:
+            admixed.write("{}\n".format(pop))
+            # Here I assume that samples are iterated over always in the same order.
+            for sample in SamplesInPop[pop]:
+               AdRefCount = record.call_for_sample[sample].data.get('GT').count('0')
+               AdAltCount = record.call_for_sample[sample].data.get('GT').count('1')
+               if AdRefCount + AdAltCount != 2:
+                  AdRefCount = -9
+                  AdAltCount = -9
+               admixed.write("{} {}\n".format(AdRefCount, AdAltCount))
+#        pop0.write("{}:{}\n{} {}\n".format(record.CHROM, record.POS, P0RefCount, P0AltCount))
+#        pop1.write("{}:{}\n{} {}\n".format(record.CHROM, record.POS, P1RefCount, P1AltCount))
+         pop0.write("locus {}\n{} {}\n".format(locus, P0RefCount, P0AltCount))
+         pop1.write("locus {}\n{} {}\n".format(locus, P1RefCount, P0AltCount))
+         locus += 1
+   pop0.close()
+   pop1.close()
+   admixed.close()
+
+if __name__ == '__main__':
+   main()
+
